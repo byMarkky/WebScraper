7b1f2be8a2475eaa91e030ab503095c17bf42b05
byMarkky <mo6368745@gmail.com>
2024-09-29 20:44:56 +0200

    Added some security for max request

--- main.py
+++ main.py
@@ -13,19 +13,27 @@ keyword_search = 'software%20development'
 geoId_Spain = '105646813'
 
 link = 'https://www.linkedin.com/jobs/search/'
-keywords = 'keywords=' + keyword_search + '&geoId=' + geoId_Spain
+keywords = 'geoId=' + geoId_Spain + '&keywords=' + keyword_search
 
 # Don't need auth in this case
 # r = requests.get(link, auth=(user, pass))
 res = requests.get(link, params=keywords, auth=(env['USER'], env['PASSWD']))
 
+# If we exceed the max request, we jump of the program
+if res.status_code == int(env['TOO_MANY_REQUEST_CODE']):
+    print(env['TOO_MANY_REQUEST_MSG'])
+    exit(1)
+    
+
 soup = BeautifulSoup(res.text, 'lxml')
 
 #with open('index.html', "w", encoding=res.encoding) as fp:
 #    fp.write(res.text)
+#    fp.close()
     
     
 jobs_container = soup.find('ul', {'class': 'jobs-search__results-list'})
+#print(jobs_container)
 jobs_lists = jobs_container.find_all('li')
 
 for list in jobs_lists:
