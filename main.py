8ee25afb6c8acbcb4d87464036a8bf66d9db2a86
byMarkky <mo6368745@gmail.com>
2024-09-30 18:35:34 +0200

    Style the code & delete some trash code

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
-from dotenv import dotenv_values
-import time
+from dotenv import load_dotenv
+import os
 import requests
 from bs4 import BeautifulSoup
 
-env = dotenv_values(".env")
+def dump_file(response, encoding):
+    with open('index.html', 'w', encoding=encoding) as file:
+        file.write(response)
+        file.close()
 
-start_time = time.time()
+def get_jobs_ids(res):
+    content = res.text
+    soup = BeautifulSoup(content, 'lxml')
+    jobs_ids = list()
 
-jobsIds = list()
+    # If we exceed the max request, we jump of the program
+    if res.status_code == int(os.getenv('TOO_MANY_REQUEST_CODE')):
+        print(res.headers)
+        print(os.getenv('TOO_MANY_REQUEST_MSG'))
+        exit(1)
+        
 
-keyword_search = 'software%20development'
-geoId_Spain = '105646813'
+    dump_file(content, res.encoding)
+        
+    jobs_container = soup.find('ul', {'class': 'jobs-search__results-list'})
+    jobs_lists = jobs_container.find_all('li')
 
-link = 'https://www.linkedin.com/jobs/search/'
-keywords = 'geoId=' + geoId_Spain + '&keywords=' + keyword_search
-
-# Don't need auth in this case
-# r = requests.get(link, auth=(user, pass))
-res = requests.get(link, params=keywords, auth=(env['USER'], env['PASSWD']))
-
-# If we exceed the max request, we jump of the program
-if res.status_code == int(env['TOO_MANY_REQUEST_CODE']):
-    print(env['TOO_MANY_REQUEST_MSG'])
-    exit(1)
+    for job in jobs_lists:
+        jobs_ids.append(job.find('div')['data-entity-urn'].split(':')[-1])
+        
+    return jobs_ids
     
 
-soup = BeautifulSoup(res.text, 'lxml')
+def main():
+    load_dotenv()
 
-#with open('index.html', "w", encoding=res.encoding) as fp:
-#    fp.write(res.text)
-#    fp.close()
-    
-    
-jobs_container = soup.find('ul', {'class': 'jobs-search__results-list'})
-#print(jobs_container)
-jobs_lists = jobs_container.find_all('li')
+    keyword_search = 'software%20development'
+    geoId_Spain = '105646813'
 
-for list in jobs_lists:
-    jobsIds.append(list.find('div')['data-entity-urn'].split(':')[-1])
-    
-end_time = time.time()
-print(jobsIds)
-print("Total time: ", (end_time - start_time), "s")
+    link = 'https://www.linkedin.com/jobs/search/'
+    keywords = 'geoId=' + geoId_Spain + '&keywords=' + keyword_search
+
+    # Don't need auth in this case
+    # r = requests.get(link, auth=(user, pass))
+    res = requests.get(link,
+                       headers={'User-agent': 'your bot 0.1'},
+                       params=keywords,
+                       auth=(os.getenv('USER'), os.getenv('PASSWD')))
+
+    jobs_ids = get_jobs_ids(res)
+    print(jobs_ids)
 
 
-    
+if __name__ == '__main__':
+    main()    
